c0b849a3deeb0f0dca143fdff5f8fcd94f23ac63
vardaro <anthonyavardaro@yahoo.com>
2017-08-06 12:33:51 -0500

    added env vars

--- wingman.js
+++ wingman.js
@@ -8,19 +8,20 @@ const { MessagingResponse } = require('twilio').twiml;
 const snoowrap = require('snoowrap');
 const app = express();
 
+const { env } = process;
 const DEFAULT = 5000;
 const OK = 200;
 const reddit = new snoowrap({
-  clientId: 'c74Q1dKouiMGNA',
-  clientSecret: 'M2UJaQvXhctXqC_7ItNLOxTpyCQ',
-  refreshToken: '33133423-kXmv7O67gfM0Ce8gp6jIbjV6Hk4',
-  userAgent: '9A25PnHxZY7nWQ'
+  clientId: env.clientId,
+  clientSecret: env.clientSecret,
+  refreshToken: env.refreshToken,
+  userAgent: env.userAgent
 });
 const client = new twitter({
-  consumer_key: 'Wl4iMW5NBRNqZJO8MYRtlzHRn',
-  consumer_secret: 'mPLoLpdc6XPwfk7spqomN3UxrOlGAfkAXpl6omf5AddztN9x69',
-  access_token_key: '2610777535-8EYut0cv8uxi6CI5lctqhgqYGgHJIISrKa5eEpH',
-  access_token_secret: '0OS1TkU3gnyqsEN2m1WtRgZPmo8zsuUVjNC9WkEyzRjzF'
+  consumer_key: env.consumer_key,
+  consumer_secret: env.consumer_secret,
+  access_token_key: env.access_token_key,
+  access_token_secret: env.access_token_secret
 });
 const tweeters = [
   'pickupIines',
@@ -46,18 +47,18 @@ app.post("/sms", function (req, res) {
 
   if (reqBody === 'shitty' || reqBody === 'help') {
     console.log('POST: SHITTY');
-    const curPlat = cut(platforms);
+    const curPlat = extract(platforms);
 
     if (curPlat === 'Reddit') {
       console.log('REDDIT');
-      const curSub = cut(subreddits);
+      const curSub = extract(subreddits);
       // grabs a subreddit
       console.log('GETTING SUB: ' + curSub);
       reddit.getSubreddit(curSub)
         .getTop({ time: 'all' })
         .then((submissions) => {
           // grab a random submission
-          const line = cut(submissions);
+          const line = extract(submissions);
           const message = twiml.message();
 
           if (line.url.includes('imgur') ||
@@ -80,14 +81,14 @@ app.post("/sms", function (req, res) {
     } else if (curPlat === 'Twitter') { // req to twitter pickuplines
       console.log('TWITTER');
       // get a user
-      const curUser = cut(tweeters);
+      const curUser = extract(tweeters);
       console.log('USER: ' + curUser);
       const request = buildReq(curUser);
       // make the request
       client.get('statuses/user_timeline', request, (err, tweet, resp) => {
         if (err) console.log('WTF');
         // get a tweet
-        const curTweet = cut(tweet);
+        const curTweet = extract(tweet);
         twiml.message(curTweet.text);
         res.writeHead(OK, { 'Content-Type': 'text/xml' });
         res.end(twiml.toString());
@@ -101,13 +102,13 @@ app.post("/sms", function (req, res) {
       .search({ query: reqBody, time: 'all', sort: 'relavance' })
       .then((submissions) => {
         if (submissions) { // check if the query returns anything
-          const curSub = cut(submissions);
+          const curSub = extract(submissions);
           reddit.getSubmission(curSub.id) // focus one 1 submission
             .expandReplies({ limit: Infinity, depth: Infinity })
             .then((withReplies) => {
               if (withReplies.comments) {
                 console.log(withReplies.comments);
-                const curRep = cut(withReplies.comments);
+                const curRep = extract(withReplies.comments);
                 const twimlResp = `${curSub.title}\n\n${curRep.body}`;
                 twiml.message(twimlResp);
               } else {
@@ -125,14 +126,14 @@ app.post("/sms", function (req, res) {
 });
 
 app.get('*', function (req, res) {
-  res.sendfile(__dirname + '/src/index.html');
+  res.sendFile(__dirname + '/src/index.html');
 });
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
 
 // returns a random object from the arra
-function cut(arr) {
+function extract(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 // returns an object of twitter api params 
