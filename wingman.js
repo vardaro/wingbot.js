60056474a83b426af9156b8b3089d0116ef6a930
vardaro <anthonyavardaro@yahoo.com>
2017-08-05 21:57:30 -0500

    twitter support!

--- wingman.js
+++ wingman.js
@@ -1,5 +1,6 @@
 'use strict'
 const express = require('express');
+const twitter = require('twitter');
 const bodyParser = require('body-parser');
 const { MessagingResponse } = require('twilio').twiml;
 const snoowrap = require('snoowrap');
@@ -13,49 +14,91 @@ const reddit = new snoowrap({
   refreshToken: '33133423-kXmv7O67gfM0Ce8gp6jIbjV6Hk4',
   userAgent: '9A25PnHxZY7nWQ'
 });
+const client = new twitter({
+  consumer_key: 'Wl4iMW5NBRNqZJO8MYRtlzHRn',
+  consumer_secret: 'mPLoLpdc6XPwfk7spqomN3UxrOlGAfkAXpl6omf5AddztN9x69',
+  access_token_key: '2610777535-8EYut0cv8uxi6CI5lctqhgqYGgHJIISrKa5eEpH',
+  access_token_secret: '0OS1TkU3gnyqsEN2m1WtRgZPmo8zsuUVjNC9WkEyzRjzF'
+});
+const statusParams = {
+  screen_name: 'pickupIines',
+  trim_user: true,
+  count: 50,
+  exclude_replies: true,
+  include_rts: false
+}
+const tweeters = [
+  'pickupIines',
+  'pickuplines',
+  'smplepickupInes'
+]
 const subreddits = [
   'ShittyPickupLines',
-  'PickupLines',
-  'Tinder'
+  'PickupLines'
+];
+const platforms = [
+  'Reddit',
+  'Twitter'
 ];
 
 app.set('port', process.env.PORT || DEFAULT);
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/src'));
 
 app.post("/sms", function (req, res) {
   const twiml = new MessagingResponse();
   const reqBody = req.body.Body.trim().toLowerCase();
 
-  if (reqBody === 'shitty') {
+  if (reqBody === 'shitty' || reqBody === 'help') {
     console.log('POST: SHITTY');
-    const curSub = derive(subreddits);
+    const curPlat = cut(platforms);
 
-    console.log('GETTING SUB: ' + curSub);
-    reddit.getSubreddit(curSub)
-      .getTop({ time: 'all' })
-      .then((submission) => {
-        const line = derive(submission);
+    if (curPlat === 'Reddit') {
+      console.log('REDDIT');
+      const curSub = cut(subreddits);
 
-        console.log(line);
-        const message = twiml.message();
-        const mesBody = line.selftext ?
-          `${line.title}\n\n${line.selftext}` :
-          `${line.title}`;
+      console.log('GETTING SUB: ' + curSub);
+      reddit.getSubreddit(curSub)
+        .getTop({ time: 'all' })
+        .then((submissions) => {
+          const line = cut(submissions);
 
-        if (line.url.includes('imgur') || line.url.includes('reddituploads') || line.url.includes('i.redd.it')) {
-          message.media(line.url);
-          console.log('IMG: ' + line.url);
-        }
-        
-        message.body(mesBody);
-        console.log(mesBody);
+          console.log(line);
+          const message = twiml.message();
+          const mesBody = line.selftext ?
+            `${line.title}\n\n${line.selftext}` :
+            `${line.title}`;
+
+          if (line.url.includes('imgur') ||
+            line.url.includes('reddituploads') ||
+            line.url.includes('i.redd.it')) {
+            message.media(line.url);
+            console.log('IMG: ' + line.url);
+          }
+
+          message.body(mesBody);
+          console.log(mesBody);
+          res.writeHead(OK, { 'Content-Type': 'text/xml' });
+          res.end(twiml.toString());
+          console.log('RESP SENT');
+        });
+    } else if (curPlat === 'Twitter') {
+      console.log('TWITTER');
+      const curUser = cut(tweeters);
+      console.log('USER: ' + curUser);
+      const request = buildReq(curUser);
+      client.get('statuses/user_timeline', request, (err, tweet, resp) => {
+        if (err) {
+          console.log('WTF');
+
+        };
+        const curTweet = cut(tweet);
+        twiml.message(curTweet.text);
         res.writeHead(OK, { 'Content-Type': 'text/xml' });
         res.end(twiml.toString());
         console.log('RESP SENT');
-      });
-  } else if (reqBody === 'help') {
-    twiml.message('helping u out');
-    res.end(twiml.toString());
+      })
+    }
   } else {
     twiml.message('girls name');
     res.end(twiml.toString());
@@ -63,11 +106,25 @@ app.post("/sms", function (req, res) {
   console.log('RES END\n');
 });
 
+app.get('*', function (req, res) {
+  res.sendfile(__dirname + '/src/index.html');
+});
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
 
 // returns a random object from the arra
-function derive(arr) {
+function cut(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
+// returns an object of twitter api params 
+// with a dynamic screen_name
+function buildReq(tweeter) {
+  return {
+    screen_name: tweeter,
+    trim_user: true,
+    count: 100,
+    exclude_replies: true,
+    include_rts: false
+  }
+} 
